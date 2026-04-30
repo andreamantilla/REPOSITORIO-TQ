9a6fa535b9a428a5a8e3c3650eca5f491960245e
jullie <jullie.mantilla@gmail.com>
2026-04-30 15:22:22 -0500

    3

--- cambios.py
+++ cambios.py
@@ -2,3 +2,11 @@ print ("commit 1")
 print ("commit 2")
 print ("commit 3")
 print ("commit 4")
+
+if __name__ == "__main__":
+    print ("commit 5")
+
+    if True:  # Replace True with your actual condition
+        print ("commit 6")
+    else:
+        print ("commit 7")
